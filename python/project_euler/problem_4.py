ce89636a0588e80fe5efcb7fdb25b8ffbbdbdb64
Yuriy Malygin <yuriy.malygin@gmail.com>
2014-10-22 04:04:47 +0400

    Python: optimize algorithm for solve Euler Problem 4

--- python/project_euler/problem_4.py
+++ python/project_euler/problem_4.py
@@ -5,21 +5,11 @@
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-x = y = z = p = 0
-
-def palindrome(x):
-
-    if (not len(x) % 2 and 
-        (x[0] == x[-1]) and 
-        (x[1] == x[-2]) and 
-        (x[2] == x[-3])):
-        return True
-    else:
-        return False
+p = 0
 
 for x in range(100, 1000):
     for y in range(100, 1000):
-        z = x * y
-        if (len(str(z)) == 6) and palindrome(str(z)) and (z > p):
+        z = str(x * y)
+        if (z[:len(z)/2] == z[len(z)/2:][::-1]) and (z > p):
             p = z
 print p
