93eb5b40a52910f901a524d70f8eb4748a9d59da
Yuriy Malygin <yuriy.malygin@gmail.com>
2014-10-26 17:33:59 +0300

    Python: optimized algorithm for solve Euler Problem 9

--- python/project_euler/problem_9.py
+++ python/project_euler/problem_9.py
@@ -7,19 +7,10 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
-def check(a,b,c,x):
-    state = False
-
-    if (a + b + c == x) and (a < b < c) and (a**2 + b**2  == c**2):
-        state = True
-
-    return state
-
 def triplet(x):
     for a in xrange(0, x):
         for b in xrange(0, x):
-            for c in xrange(0, x):
-                if check(a,b,c,x):
-                    return a, b, c
+            if (a < b < (x - a - b)) and (a**2 + b**2  == (x - a - b)**2):
+                return a * b * (x - a - b)
 
 print triplet(1000)
